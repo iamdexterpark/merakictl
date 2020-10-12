636633cd9b180da9297a9f23c3683570bed639d2
“Dexter <ddexterpark@icloud.com>
2020-10-11 20:36:57 -0500

    fix: missing parameters

--- api/general/networks/monitor/splash.go
+++ api/general/networks/monitor/splash.go
@@ -20,10 +20,19 @@ type SplashLogin []struct {
 }
 
 // List the splash login attempts for a network
-func GetSplashLoginAttempts(networkId string) (SplashLogin, interface{}) {
+func GetSplashLoginAttempts(networkId, splashLoginAttempts, ssidNumber, loginIdentifier, timespan string) (SplashLogin, interface{}) {
 	baseurl := fmt.Sprintf("%s/networks/%s/splashLoginAttempts", api.BaseUrl(), networkId)
 	var payload io.ReadSeeker
 	session := api.Session(baseurl, "GET", payload)
+
+	// Parameters for Request URL
+	parameters := session.Request.URL.Query()
+	parameters.Add("splashLoginAttempts", splashLoginAttempts)
+	parameters.Add("ssidNumber", ssidNumber)
+	parameters.Add("loginIdentifier", loginIdentifier)
+	parameters.Add("timespan", timespan)
+	session.Request.URL.RawQuery = parameters.Encode()
+
 	var results = SplashLogin{}
 	user_agent.UnMarshalJSON(session.Body, &results)
 	traceback := user_agent.TraceBack(session)
