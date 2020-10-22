ea78ab7355e6fee48435a1ad1014c45530586f49
“Dexter <ddexterpark@icloud.com>
2020-10-21 21:48:49 -0500

    fix: move org api endpoints

--- api/general/networks/configure/networks.go
+++ api/general/networks/configure/networks.go
@@ -7,55 +7,6 @@ import (
 	"io"
 )
 
-type Clients []struct {
-	Usage struct {
-		Sent int `json:"sent"`
-		Recv int `json:"recv"`
-	} `json:"usage"`
-	ID                 string      `json:"id"`
-	Description        string      `json:"description"`
-	Mac                string      `json:"mac"`
-	IP                 string      `json:"ip"`
-	User               string      `json:"user"`
-	Vlan               int         `json:"vlan"`
-	Switchport         interface{} `json:"switchport"`
-	IP6                string      `json:"ip6"`
-	FirstSeen          int         `json:"firstSeen"`
-	LastSeen           int         `json:"lastSeen"`
-	Manufacturer       string      `json:"manufacturer"`
-	Os                 string      `json:"os"`
-	RecentDeviceSerial string      `json:"recentDeviceSerial"`
-	RecentDeviceName   string      `json:"recentDeviceName"`
-	RecentDeviceMac    string      `json:"recentDeviceMac"`
-	Ssid               string      `json:"ssid"`
-	Status             string      `json:"status"`
-	Notes              string      `json:"notes"`
-	IP6Local           string      `json:"ip6Local"`
-	SmInstalled        bool        `json:"smInstalled"`
-	GroupPolicy8021X   string      `json:"groupPolicy8021x"`
-}
-
-// GetOrganizations - List the organizations that the user has privileges on
-func GetNetworkClients(networkId, t0, t1, timespan, perPage, startingAfter, endingBefore string) (Clients, interface{}) {
-	baseurl := fmt.Sprintf("%s/networks/%s/clients", api.BaseUrl(), networkId)
-	var payload io.ReadSeeker
-	session := api.Session(baseurl, "GET", payload)
-
-	// Parameters for Request URL
-	parameters := session.Request.URL.Query()
-	parameters.Add("t0", t0)
-	parameters.Add("t1", t1)
-	parameters.Add("timespan", timespan)
-	parameters.Add("perPage",perPage)
-	parameters.Add("startingAfter",startingAfter)
-	parameters.Add("endingBefore", endingBefore)
-	session.Request.URL.RawQuery = parameters.Encode()
-
-	var results = Clients{}
-	user_agent.UnMarshalJSON(session.Body, &results)
-	traceback := user_agent.TraceBack(session)
-	return results, traceback
-}
 
 
 type Network struct {
