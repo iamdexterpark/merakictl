e7f5e0afe8a0f0ce7ed09c2b6c02da95e2289b3a
“Dexter <ddexterpark@icloud.com>
2020-10-27 08:44:27 -0600

    feat: GetAggregatedConnectivityPerNetwork

--- api/general/networks/monitor/connectionstats.go
+++ api/general/networks/monitor/connectionstats.go
@@ -42,6 +42,37 @@ func GetAPAggregatedConnectivity(serial, t0, t1, timespan,
 	traceback := user_agent.TraceBack(session)
 	return results, traceback
 }
-// Aggregated Connectivity Info For This Network
 
+type AggregatedConnectivityPerNetwork struct {
+	Assoc   int `json:"assoc"`
+	Auth    int `json:"auth"`
+	Dhcp    int `json:"dhcp"`
+	DNS     int `json:"dns"`
+	Success int `json:"success"`
+}
+
+// Aggregated Connectivity Info For This Network
+func GetAggregatedConnectivityPerNetwork(networkId, t0, t1, timespan,
+	band, ssid, vlan, apTag, fields string) (AggregatedConnectivityPerNetwork, interface{}) {
+	baseurl := fmt.Sprintf("%s/networks/%s/wireless/connectionStats",
+		api.BaseUrl(), networkId)
+	var payload io.ReadSeeker
+	session := api.Session(baseurl, "GET", payload)
+
+	// Parameters for Request URL
+	parameters := session.Request.URL.Query()
+	parameters.Add("t0", t0)
+	parameters.Add("t1", t1)
+	parameters.Add("timespan", timespan)
+	parameters.Add("band", band)
+	parameters.Add("ssid", ssid)
+	parameters.Add("vlan", vlan)
+	parameters.Add("apTag", apTag)
+	session.Request.URL.RawQuery = parameters.Encode()
+
+	var results = AggregatedConnectivityPerNetwork{}
+	user_agent.UnMarshalJSON(session.Body, &results)
+	traceback := user_agent.TraceBack(session)
+	return results, traceback
+}
 // Return PHY data rates over time for a network, device, or network client
