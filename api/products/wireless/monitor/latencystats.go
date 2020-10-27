ae141ddf27d169c13fcc8e741154ce15403fd033
“Dexter <ddexterpark@icloud.com>
2020-10-27 09:28:53 -0600

    feat: GetAggregatedLatencyPerNetwork

--- api/products/wireless/monitor/latencystats.go
+++ api/products/wireless/monitor/latencystats.go
@@ -60,4 +60,52 @@ func GetAggregatedLatencyPerAP(serial, t0, t1, timespan,
 	return results, traceback
 }
 
+type AggregatedLatencyPerNetwork struct {
+	BackgroundTraffic struct {
+		RawDistribution struct {
+			Num0    int `json:"0"`
+			Num1    int `json:"1"`
+			Num2    int `json:"2"`
+			Num4    int `json:"4"`
+			Num8    int `json:"8"`
+			Num16   int `json:"16"`
+			Num32   int `json:"32"`
+			Num64   int `json:"64"`
+			Num128  int `json:"128"`
+			Num256  int `json:"256"`
+			Num512  int `json:"512"`
+			Num1024 int `json:"1024"`
+			Num2048 int `json:"2048"`
+		} `json:"rawDistribution"`
+		Avg float64 `json:"avg"`
+	} `json:"backgroundTraffic"`
+	BestEffortTraffic string `json:"bestEffortTraffic"`
+	VideoTraffic      string `json:"videoTraffic"`
+	VoiceTraffic      string `json:"voiceTraffic"`
+}
+
 // Aggregated Latency Info For This Network
+func GetAggregatedLatencyPerNetwork(networkId, t0, t1, timespan,
+	band, ssid, vlan, apTag, fields string) (AggregatedLatencyPerNetwork, interface{}) {
+	baseurl := fmt.Sprintf("%s/networks/%s/wireless/clients/latencyStats",
+		api.BaseUrl(), networkId)
+	var payload io.ReadSeeker
+	session := api.Session(baseurl, "GET", payload)
+
+	// Parameters for Request URL
+	parameters := session.Request.URL.Query()
+	parameters.Add("t0", t0)
+	parameters.Add("t1", t1)
+	parameters.Add("timespan", timespan)
+	parameters.Add("band", band)
+	parameters.Add("ssid", ssid)
+	parameters.Add("vlan", vlan)
+	parameters.Add("apTag", apTag)
+	parameters.Add("fields", fields)
+	session.Request.URL.RawQuery = parameters.Encode()
+
+	var results = AggregatedLatencyPerNetwork{}
+	user_agent.UnMarshalJSON(session.Body, &results)
+	traceback := user_agent.TraceBack(session)
+	return results, traceback
+}
